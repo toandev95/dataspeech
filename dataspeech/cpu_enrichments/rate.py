d3a1bcc4edcd1d3c768cdf48ba1c886c7471f19b
Toan Doan <toandev.95@gmail.com>
2025-01-14 21:50:07 +0700

    Update rate.py

--- dataspeech/cpu_enrichments/rate.py
+++ dataspeech/cpu_enrichments/rate.py
@@ -1,4 +1,18 @@
 from phonemizer import phonemize
+from phonemizer.separator import Separator
+
+
+def phonemize_vi(text):
+    return phonemize(
+        text,
+        language="vi",
+        backend="espeak",
+        separator=Separator(phone=None, word=" ", syllable="|"),
+        strip=True,
+        preserve_punctuation=True,
+        njobs=4,
+        language_switch="remove-flags",
+    )
 
 
 def rate_apply(batch, rank=None, audio_column_name="audio", text_column_name="text"):
@@ -9,14 +23,14 @@ def rate_apply(batch, rank=None, audio_column_name="audio", text_column_name="te
             for text, audio_duration in zip(
                 batch[text_column_name], batch["speech_duration"]
             ):
-                phonemes = phonemize(text, language="vi")
+                phonemes = phonemize_vi(text)
                 audio_duration = audio_duration if audio_duration != 0 else 0.01
                 speaking_rate = len(phonemes) / audio_duration
                 speaking_rates.append(speaking_rate)
                 phonemes_list.append(phonemes)
         else:
             for text, audio in zip(batch[text_column_name], batch[audio_column_name]):
-                phonemes = phonemize(text, language="vi")
+                phonemes = phonemize_vi(text)
 
                 sample_rate = audio["sampling_rate"]
                 audio_length = len(audio["array"].squeeze()) / sample_rate
@@ -29,7 +43,7 @@ def rate_apply(batch, rank=None, audio_column_name="audio", text_column_name="te
         batch["speaking_rate"] = speaking_rates
         batch["phonemes"] = phonemes_list
     else:
-        phonemes = phonemize(batch[text_column_name], language="vi")
+        phonemes = phonemize_vi(batch[text_column_name])
         if "speech_duration" in batch:
             audio_length = (
                 batch["speech_duration"] if batch["speech_duration"] != 0 else 0.01
